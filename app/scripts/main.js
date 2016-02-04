9be94ac8a0d0c1f794d2a71161838cc25d243769
dwyerb64 <dwyerb@tcd.ie>
2016-02-04 21:17:57 +0000

    initial commit with logo on canvas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,66 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+'use strict';
+
+var SEMICOLON = SEMICOLON || {};
+
+(function($){
+    
+  //               //
+ //  FUNCTIONS    //
+//               //
+SEMICOLON.documentOnReady = {
+    init: function(){
+
+	  var canvas = document.getElementById('landingCanvas');
+      var context = canvas.getContext('2d');
+      var imageObj = new Image();
+
+      imageObj.onload = function() {
+        imageObj.width = canvas.width;
+        imageObj.height = canvas.height;
+        context.globalAlpha = 0.7;
+        context.drawImage(imageObj, 30, 0);
+        context.globalAlpha = 1.0;
+        context.fillStyle="#000000";
+      };
+      imageObj.src = '/images/no_more.png';
+
+      canvas.width = $window.width();
+	  canvas.height = $window.height();
+    },
+    
+    windowscroll: function() {
+        
+
+    }
+
+};
+
+SEMICOLON.functions = {
+    
+    functionName: function(){
+        
+  }
+};
+
+
+
+  //               //
+ //  DOM objects  //
+//               //
+var $htmlBody = $('body,html'),
+    $window = $(window),
+    isMobDevice = (/iphone|ipad|Android|webOS|iPod|BlackBerry|Windows Phone|ZuneWP7/gi).test(navigator.appVersion);
+
+
+  //          //
+ //  Events  //
+//          //
+$(document).ready( SEMICOLON.documentOnReady.init );
+
+$(document).scroll( SEMICOLON.documentOnReady.windowscroll );
+        
+        
+})(jQuery);
+
