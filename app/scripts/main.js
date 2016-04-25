bc27a09a0a3cfe101c1bd88164aa70f96cc0f9fb
dwyerb64 <briandwyer@MacBook-Pro.local>
2016-04-25 12:47:15 +0100

    Working version of mobile and desktop. Uses canvas for pixel colour. tested in chrome

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,8 +12,35 @@ var SEMICOLON = SEMICOLON || {};
 SEMICOLON.documentOnReady = {
     init: function(){
 
-	  if(!isMobDevice){ // CHECK FOR MODERNIZR VARIABLE
+        if(!isMobDevice || !Modernizr.touch){ 
 
+            var video = SEMICOLON.functions.setUpVideo();
+
+            $('#landing #videoContainerDiv').append(video);
+            // landingSection.appendChild(video);
+
+            canvas.width = $window.width();
+            canvas.height = $window.height();
+
+            video.play();
+            video.ontimeupdate = SEMICOLON.functions.checkVideoTime;
+
+        }
+
+        $('#landing').addClass('js-loaded').fadeIn(500);
+
+    },
+    
+    windowscroll: function() {
+        
+
+    }
+
+};
+
+SEMICOLON.functions = {
+    
+    setUpVideo: function(){
         var video = document.createElement('video');
         video.poster = "images/no-more-backgrounds.jpg";
         video.preload = "auto";
@@ -28,67 +55,9 @@ SEMICOLON.documentOnReady = {
             video.src = 'images/no-more-backgrounds3.mp4';
         }
 
-        // var landingSection = document.getElementById('landing');
-        $('#landing #videoContainerDiv').append(video);
-        // landingSection.appendChild(video);
-
-        canvas.width = $window.width();
-        canvas.height = $window.height();
-
-        video.play();
-        video.ontimeupdate = SEMICOLON.functions.checkVideoTime;
-
-
-            // add html 5 video
-            // $("#videoContainer").html(
-            //                 '<video id="videoObj" poster="images/no-more-backgrounds.jpg" preload="auto" loop autoplay muted>' +
-            //                     // '<source src="images/video/Greenfinch Video Background_6.webm" type="video/webm"></source>' +
-            //                     '<source src="images/no-more-backgrounds3.mp4" type="video/mp4"></source>' +
-            //                     // '<source src="images/video/Greenfinch Video Background_6.ogg" type="video/ogg"></source>' +
-            //                 '</video>' +
-            //                 '<div class="video-overlay" style="background-color: rgba(173, 173, 173, 0.0);"></div>');
-            // $("#videoContainer").fadeIn();
-
-
-            // var videoObj = document.getElementById('videoObj');
-
-            // SEMICOLON.functions.addVideoToCanvas(video);
-
-
-        }
+        return video;  
 
     },
-    
-    windowscroll: function() {
-        
-
-    }
-
-};
-
-SEMICOLON.functions = {
-    
-    setUpVideo: function(video){
-        // var videoElement = document.getElementById('v');
-        
-        // var context = canvas.getContext('2d');
-
-
-
-        
-        // this.addVideoToCanvas(video,context);
-        
-
-    },
-
-    // addVideoToCanvas: function(video){  
-
-        
-    //     setTimeout(
-    //         SEMICOLON.functions.addVideoToCanvas,1000/25,video
-    //         );
-    // }
-    // ,
 
   checkVideoTime: function(){
 
@@ -112,11 +81,8 @@ SEMICOLON.functions = {
                         black++;
                     }
 
-                    // pix[j+2] = 255; // blue
-
-                    // i+3 is alpha (the fourth element)
                 }
-                // ctx.putImageData(imgd,iconPosition.left  + 50,iconPosition.top  - 50);
+                
 
                 if(black > white){
                     $('.icon-' + i + ' .social-icon').addClass('white-icon');
@@ -124,19 +90,6 @@ SEMICOLON.functions = {
                     $('.icon-' + i + ' .social-icon').removeClass('white-icon');
                 }
             };
-
-
-        // if(this.currentTime > 14){
-        //     // back to ink
-        //     $('.social-icon').removeClass('white-icon').addClass('red-icon');
-        // }else if(this.currentTime > 12){
-        //     // jellyfish
-        //     $('.social-icon').addClass('white-icon');
-        // }else if(this.currentTime > 6){
-        //     for (var i = 3; i >= 0; i--) {
-        //         $('.icon-' + i + ' .social-icon').addClass('white-icon');
-        //     };
-        // }
         
   },
 
